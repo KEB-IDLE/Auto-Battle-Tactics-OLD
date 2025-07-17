edbfedd285c1006ede2069ac5986df4ebc7d3586
Langdongit <ibmsp01@gmail.com>
2025-07-17 14:37:52 +0900

    update: 소리 기능 추가

--- Assets/Scripts/Game/EntityComponent/AnimationComponent.cs
+++ Assets/Scripts/Game/EntityComponent/AnimationComponent.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent (typeof(AudioSource))]
 public class AnimationComponent : MonoBehaviour
 {
     private Animator _animator;
     private AnimatorOverrideController _controller;
+    private AudioSource _audioSource;
+    private AudioClip summonSound;
+    private AudioClip attackSound;
+    private AudioClip deathSound;
 
     IAttackNotifier  _attackSrc;
     IMoveNotifier    _moveSrc;
@@ -16,23 +21,37 @@ public class AnimationComponent : MonoBehaviour
         _attackSrc = GetComponent<IAttackNotifier>();
         _moveSrc = GetComponent<IMoveNotifier>();
         _deathSrc = GetComponent<IDeathNotifier>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void Initialize(EntityData data)
     {
         if (_controller == null)
             _controller = new AnimatorOverrideController(data.animatorController);
-
         _animator.runtimeAnimatorController = _controller;
 
-        if (data.attackClip != null)
-            _controller["Attack"] = data.attackClip;
-        if (data.runClip != null)
-            _controller["Run"] = data.runClip;
-        if (data.deathClip != null)
-            _controller["Die"] = data.deathClip;
-        if (data.idleClip != null)
-            _controller["Idle"] = data.idleClip;
+        try
+        {
+            if (data.attackClip != null)
+                _controller["Attack"] = data.attackClip;
+            if (data.runClip != null)
+                _controller["Run"] = data.runClip;
+            if (data.deathClip != null)
+                _controller["Die"] = data.deathClip;
+            if (data.idleClip != null)
+                _controller["Idle"] = data.idleClip;
+
+            if (data.summonSound != null)
+                this.summonSound = data.summonSound;
+            if (data.attackSound != null)
+                this.attackSound = data.attackSound;
+            if (data.deathSound != null)
+                this.deathSound = data.deathSound;
+        }
+        catch
+        {
+            Debug.LogError("Clip / Sound 설정이 되어있지 않음");
+        }
     }
 
     public void Bind()
@@ -55,6 +74,7 @@ public class AnimationComponent : MonoBehaviour
         {
             Debug.Log("attack Start!");
             _animator.SetBool("Attack", true);
+            _audioSource.PlayOneShot(attackSound);
         }
         else
         {
@@ -72,6 +92,7 @@ public class AnimationComponent : MonoBehaviour
     void HandleDeath()
     {
         _animator.SetTrigger("Die");
+        _audioSource.PlayOneShot(deathSound);
         Unbind();
     }
 }
