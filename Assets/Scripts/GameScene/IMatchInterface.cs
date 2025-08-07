3b7c70e9de6922f400730fd0275000d71262be68
tmdgur23 <dltmdgur08@naver.com>
2025-08-07 11:42:35 +0900

    매칭->게임씬 구현

--- Assets/Scripts/GameScene/IMatchInterface.cs
+++ Assets/Scripts/GameScene/IMatchInterface.cs
@@ -2,6 +2,7 @@ using System.Collections;
 
 public interface IMatchInterface
 {
-    IEnumerator StartMatchFlow();
-    IEnumerator EndMatchFlow();
+    int UserId { get; }                  // ✅ SessionManager에서 가져오기
+    IEnumerator StartMatchFlow();       // 매칭 시작
+    IEnumerator EndMatchFlow();         // 매칭 종료
 }
