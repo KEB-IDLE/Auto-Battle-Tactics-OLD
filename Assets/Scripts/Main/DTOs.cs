bcf8961be3787cbb6b3753056a0b6585fd1da669
jaesik <ojs72272@gmail.com>
2025-08-18 09:54:23 +0900

    fix: pjt setting

---
+++ Assets/Scripts/Main/DTOs.cs
@@ -0,0 +1,5 @@
+/// <summary>
+/// dto.cs 파일에는 클라이언트와 서버 간 데이터 교환을 위한 모든 DTO(Data Transfer Object)가 정의되어 있습니다.
+/// 이 파일의 DTO들은 모두 [System.Serializable]을 사용하여 Unity에서 직렬화가 가능하며,
+/// 서버와 클라이언트 간 HTTP 요청/응답 혹은 JSON 데이터 변환에 활용됩니다.
+/// </summary>
